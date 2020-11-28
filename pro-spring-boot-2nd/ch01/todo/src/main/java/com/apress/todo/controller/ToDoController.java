1bc685ae413deea0129d8e8a64229aa2691e903a
morzianka <pistazie@gmx.com>
2020-11-28 17:01:39 +0300

    fix

--- pro-spring-boot-2nd/ch01/todo/src/main/java/com/apress/todo/controller/ToDoController.java
+++ pro-spring-boot-2nd/ch01/todo/src/main/java/com/apress/todo/controller/ToDoController.java
@@ -3,7 +3,6 @@ package com.apress.todo.controller;
 
 import com.apress.todo.domain.ToDo;
 import com.apress.todo.repository.ToDoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,12 +17,10 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping("/")
 public class ToDoController {
 
-    private ToDoRepository repository;
+    private final ToDoRepository repository;
 
-    @Autowired
     public ToDoController(ToDoRepository repository) {
         this.repository = repository;
     }
@@ -35,9 +32,9 @@ public class ToDoController {
     }
 
     @RequestMapping(value = "/toDos", method = { RequestMethod.GET }, produces = {
-            MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_XML_VALUE})
+            MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_XML_VALUE})
     public ResponseEntity<Iterable<ToDo>> getToDos(@RequestHeader HttpHeaders headers) {
-        return new ResponseEntity<Iterable<ToDo>>(this.repository.findAll(), headers, HttpStatus.OK);
+        return new ResponseEntity<>(this.repository.findAll(), headers, HttpStatus.OK);
     }
 
 }
